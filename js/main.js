03f6c055c22a527d96a643ce27bd61f98762c85c
Cherkas Ivan <63066511+CherkasIvan@users.noreply.github.com>
2020-08-07 17:54:15 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var instance = new vidbg(".video", {
+  mp4: 'video/world.mp4', // URL or relative path to MP4 video
+  webm: 'video/world.webm', // URL or relative path to webm video
+  poster: 'video/poster.jpg', // URL or relative path to fallback image
+  overlay: false, // Boolean to display the overlay or not
+});
+var rellax = new Rellax(".rocket");
+if (document.body.clientWidth < 576) {
+  rellax.destroy();
+};
+AOS.init();
